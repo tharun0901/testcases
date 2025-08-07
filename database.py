e590fd8bd449d7d81ab98d08a6ecb5656242defc
Tharun0901 <moodtharu9@gmail.com>
2025-08-07 11:46:33 +0530

    developed ci/cd

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-URL_DATABASE = "mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+URL_DATABASE = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(URL_DATABASE)
 
