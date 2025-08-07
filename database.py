c3bbcd4d830fcc0bd061200f71e5a4e727424068
Tharun0901 <moodtharu9@gmail.com>
2025-08-07 11:24:37 +0530

    developed ci/cd

--- database.py
+++ database.py
@@ -2,8 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
-URL_DATABASE = "mysql+pymysql://root:tharun@localhost:3306/yourdatabase"
+URL_DATABASE = "mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(URL_DATABASE)
 
